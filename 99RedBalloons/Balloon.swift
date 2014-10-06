eaf82ed98f9c1168d52fbfca044ead078dc89452
Macbook Pro Video <macbookprovideo@MacBook-Pro-de-Macbook.local>
2014-10-06 16:44:28 +0200

    first version of the 99RedBalloons challenge

---
+++ 99RedBalloons/Balloon.swift
@@ -0,0 +1,9 @@
+//
+//  Balloon.swift
+//  99RedBalloons
+//
+//  Created by Victor Martinez on 03/10/14.
+//  Copyright (c) 2014 Victor Mart. All rights reserved.
+//
+
+import Foundation
---
+++ 99RedBalloons/Explosion.swift
@@ -0,0 +1,9 @@
+//
+//  Explosion.swift
+//  99RedBalloons
+//
+//  Created by Victor Martinez on 03/10/14.
+//  Copyright (c) 2014 Victor Mart. All rights reserved.
+//
+
+import Foundation
